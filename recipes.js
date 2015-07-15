e990acd7fb3bdd9c57271bd26484a171c2525f34
Charlie Briggs <charles.briggs@telefonica.com>
2015-07-16 00:11:22 +0100

    Component: Babel
    Added require hook (http://babeljs.io/docs/usage/require/) to allow
    running using node rather than babel-node.

--- recipes.js
+++ recipes.js
@@ -1,5 +1,6 @@
 // base package at root level to start the application server
 // this is used to allow relative paths to be resolved from the root dir
-const app = require('./src/app.js')
+require("babel/register");
+var app = require('./src/app.js')
 
 app.start();
